0fe6435431d212ace3901c9185e233868c4caa52
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2022-02-10 22:23:57 -0800

    feat: initial version

---
+++ index.js
@@ -0,0 +1,58 @@
+module.exports = {
+  success,
+};
+
+const { Octokit } = require("@octokit/core");
+const debug = require("debug")(
+  "semantic-release:semantic-release-breaking-version-tag"
+);
+
+const parseGithubUrl = require("./parse-github-url.js");
+
+async function success(
+  _pluginConfig,
+  { env, nextRelease, options: { repositoryUrl } }
+) {
+  const octokit = new Octokit({
+    auth: env.GH_TOKEN || env.GITHUB_TOKEN,
+  });
+  const { owner, repo } = parseGithubUrl(repositoryUrl);
+
+  const [breakingVersion] = nextRelease.version.split(".");
+  const breakingVersionTag = `v${breakingVersion}`;
+  const sha = nextRelease.gitHead;
+
+  let exists;
+  try {
+    // https://docs.github.com/en/rest/reference/git#get-a-reference
+    exists = await octokit.request("GET /repos/{owner}/{repo}/git/ref/{ref}", {
+      owner,
+      repo,
+      ref: `tags/${breakingVersionTag}`,
+    });
+
+    debug(`tag ${breakingVersionTag} already exists`);
+
+    // https://docs.github.com/en/rest/reference/git#update-a-reference
+    await octokit.request("PATCH /repos/{owner}/{repo}/git/refs/{ref}", {
+      owner,
+      repo,
+      ref: `tags/${breakingVersionTag}`,
+      sha,
+      force: true,
+    });
+  } catch (error) {
+    /* c8 ignore next */
+    if (error.status !== 404) throw error;
+
+    debug(`tag ${breakingVersionTag} does not exist yet`);
+
+    // https://docs.github.com/en/rest/reference/git#create-a-reference
+    await octokit.request("POST /repos/{owner}/{repo}/git/refs", {
+      owner,
+      repo,
+      sha,
+      ref: `refs/tags/${breakingVersionTag}`,
+    });
+  }
+}
