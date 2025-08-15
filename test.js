cdd09d495f644d769e010630edeeda74de982002
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2025-08-15 13:18:08 -0700

    feat: support premajor releases (e.g. v2-beta) (#2)

--- test.js
+++ test.js
@@ -7,7 +7,7 @@ test("create new v1 tag", async (t) => {
   const mock = nock("https://api.github.com")
     // tag does not yet exist
     .get(
-      "/repos/gr2m/semantic-release-plugin-github-breaking-version-tag/git/ref/tags%2Fv1"
+      "/repos/gr2m/semantic-release-plugin-github-breaking-version-tag/git/ref/tags%2Fv1",
     )
     .reply(404)
     // create tag
@@ -17,7 +17,7 @@ test("create new v1 tag", async (t) => {
         t.deepEqual(body, { ref: "refs/tags/v1", sha: "sha123" });
 
         return true;
-      }
+      },
     )
     .reply(201);
 
@@ -27,12 +27,12 @@ test("create new v1 tag", async (t) => {
       env: {
         GITHUB_TOKEN: "token",
       },
-      nextRelease: { version: "1.2.3", gitHead: "sha123" },
+      nextRelease: { type: "major", version: "1.2.3", gitHead: "sha123" },
       options: {
         repositoryUrl:
           "https://github.com/gr2m/semantic-release-plugin-github-breaking-version-tag",
       },
-    }
+    },
   );
 
   t.deepEqual(mock.pendingMocks(), []);
@@ -42,7 +42,7 @@ test("update existing v1 new tag", async (t) => {
   const mock = nock("https://api.github.com")
     // tag does not yet exist
     .get(
-      "/repos/gr2m/semantic-release-plugin-github-breaking-version-tag/git/ref/tags%2Fv1"
+      "/repos/gr2m/semantic-release-plugin-github-breaking-version-tag/git/ref/tags%2Fv1",
     )
     .reply(200, {
       object: {
@@ -55,7 +55,7 @@ test("update existing v1 new tag", async (t) => {
       (body) => {
         t.deepEqual(body, { sha: "sha123", force: true });
         return true;
-      }
+      },
     )
     .reply(200);
 
@@ -65,12 +65,71 @@ test("update existing v1 new tag", async (t) => {
       env: {
         GITHUB_TOKEN: "token",
       },
-      nextRelease: { version: "1.2.3", gitHead: "sha123" },
+      nextRelease: { type: "major", version: "1.2.3", gitHead: "sha123" },
       options: {
         repositoryUrl:
           "https://github.com/gr2m/semantic-release-plugin-github-breaking-version-tag",
       },
-    }
+    },
+  );
+
+  t.deepEqual(mock.pendingMocks(), []);
+});
+
+test("ignores non-major releases", async (t) => {
+  const mock = nock("https://api.github.com");
+
+  await success(
+    {},
+    {
+      env: {
+        GITHUB_TOKEN: "token",
+      },
+      nextRelease: { type: "minor", version: "1.2.3", gitHead: "sha123" },
+      options: {
+        repositoryUrl:
+          "https://github.com/gr2m/semantic-release-plugin-github-breaking-version-tag",
+      },
+    },
+  );
+
+  t.deepEqual(mock.pendingMocks(), []);
+});
+
+test("create new v2-beta tag", async (t) => {
+  const mock = nock("https://api.github.com")
+    // tag does not yet exist
+    .get(
+      "/repos/gr2m/semantic-release-plugin-github-breaking-version-tag/git/ref/tags%2Fv2-beta",
+    )
+    .reply(404)
+    // create tag
+    .post(
+      "/repos/gr2m/semantic-release-plugin-github-breaking-version-tag/git/refs",
+      (body) => {
+        t.deepEqual(body, { ref: "refs/tags/v2-beta", sha: "sha123" });
+        return true;
+      },
+    )
+    .reply(201);
+
+  await success(
+    {},
+    {
+      env: {
+        GITHUB_TOKEN: "token",
+      },
+      nextRelease: {
+        type: "premajor",
+        version: "2.0.0-beta.1",
+        gitHead: "sha123",
+        channel: "beta",
+      },
+      options: {
+        repositoryUrl:
+          "https://github.com/gr2m/semantic-release-plugin-github-breaking-version-tag",
+      },
+    },
   );
 
   t.deepEqual(mock.pendingMocks(), []);
