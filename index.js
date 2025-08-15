b46dc93117990a305f6277af7cd391d1f28d0574
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2025-08-15 13:12:21 -0700

    feat: support premajor releases (e.g. v2-beta)

--- index.js
+++ index.js
@@ -4,32 +4,46 @@ module.exports = {
 
 const { Octokit } = require("@octokit/core");
 const debug = require("debug")(
-  "semantic-release:semantic-release-plugin-github-breaking-version-tag"
+  "semantic-release:semantic-release-plugin-github-breaking-version-tag",
 );
 
 const parseGithubUrl = require("./parse-github-url.js");
 
 async function success(
   _pluginConfig,
-  { env, nextRelease, options: { repositoryUrl } }
+  { env, nextRelease, options: { repositoryUrl } },
 ) {
+  const isPreMajor = nextRelease.type === "premajor";
+  const isMajor = nextRelease.type === "major";
+
+  if (!isPreMajor && !isMajor) {
+    debug("next release is not a major version, skipping tag creation");
+    return;
+  }
+
   const octokit = new Octokit({
     auth: env.GH_TOKEN || env.GITHUB_TOKEN,
   });
   const { owner, repo } = parseGithubUrl(repositoryUrl);
 
   const [breakingVersion] = nextRelease.version.split(".");
-  const breakingVersionTag = `v${breakingVersion}`;
+  let breakingVersionTag = `v${breakingVersion}`;
   const sha = nextRelease.gitHead;
 
-  let exists;
+  if (isPreMajor) {
+    breakingVersionTag += `-${nextRelease.channel}`;
+  }
+
   try {
     // https://docs.github.com/en/rest/reference/git#get-a-reference
-    exists = await octokit.request("GET /repos/{owner}/{repo}/git/ref/{ref}", {
-      owner,
-      repo,
-      ref: `tags/${breakingVersionTag}`,
-    });
+    const exists = await octokit.request(
+      "GET /repos/{owner}/{repo}/git/ref/{ref}",
+      {
+        owner,
+        repo,
+        ref: `tags/${breakingVersionTag}`,
+      },
+    );
 
     debug(`tag ${breakingVersionTag} already exists`);
 
